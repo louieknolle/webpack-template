5df9db867b0e94c4737af34773ec580f7c84f809
Louie Knolle <91914975+louieknolle@users.noreply.github.com>
2022-05-22 17:53:03 -0400

    add template for api call

--- src/index.js
+++ src/index.js
@@ -7,3 +7,17 @@ import './css/styles.css';
 $(document).ready(function() {
   //UI Logic goes here
 });
+
+/* sample API request
+let request = new XMLHttpRequest();
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${state}&appid=${process.env.API_KEY}`;
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send(); */
